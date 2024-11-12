945d3dadf347d8b0218292e4c2245e3aa71a75a1
Miguel <lomelyo@gmail.com>
2024-11-12 03:26:12 -0600

    Structure of the API Package

---
+++ NewsApi/EnvData.swift
@@ -0,0 +1,12 @@
+//
+//  EnvData.swift
+//  NewsApi
+//
+//  Created by Miguel A Lomeli Cantu on 12/11/24.
+//
+
+struct EnvData {
+    static let URL_PREFIX = "https://"
+    static let HOST = "newsapi.org"
+    static var apiKey = ""
+}
